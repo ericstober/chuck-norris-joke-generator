7e32b07b85afa35f577cb55d6da167efb276331d
ericstober <erictstober@gmail.com>
2024-03-04 15:41:11 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const jokeElement = document.getElementById('joke');
+const jokeBtn = document.getElementById('joke-btn');
+
+const generateJoke = () => {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'https://api.chucknorris.io/jokes/random');
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4) {
+      if (this.status === 200) {
+        jokeElement.innerHTML = JSON.parse(this.responseText).value;
+      } else {
+        jokeElement.innerHTML = 'Something Went Wrong (Not Funny)'
+      }
+    }
+  }
+
+  xhr.send();
+}
+
+jokeBtn.addEventListener('click', generateJoke);
+document.addEventListener('DOMContentLoaded', generateJoke);
